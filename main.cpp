c604270b6bf93249c75f623759b89e6f171d18bd
j516143@gmail.com <j516143@gmail.com>
2020-06-03 17:04:40 +0800

    add

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include "mbed.h"
 
+#include "MQTTNetwork.h"
+#include "MQTTmbed.h"
+#include "MQTTClient.h"
+
 #include "mbed_rpc.h"
 
 #include "fsl_port.h"
@@ -46,6 +50,47 @@
 
 #define FXOS8700Q_WHOAMI_VAL 0xC7
 
+WiFiInterface *wifi;
+volatile int message_num = 0;
+volatile int arrivedcount = 0;
+volatile bool closed = false;
+const char* topic = "Mbed";
+
+Thread mqtt_thread(osPriorityHigh);
+EventQueue mqtt_queue;
+
+void messageArrived(MQTT::MessageData& md) {
+      MQTT::Message &message = md.message;
+      char msg[300];
+      sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
+      printf(msg);
+      wait_ms(1000);
+      char payload[300];
+      sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
+      printf(payload);
+      ++arrivedcount;
+}
+
+void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
+      message_num++;
+      MQTT::Message message;
+      char buff[100];
+      sprintf(buff, "QoS0 Hello, Python! #%d", message_num);
+      sprintf(buff, "Velocity = %1.4f\r\n", velocity );
+      message.qos = MQTT::QOS0;
+      message.retained = false;
+      message.dup = false;
+      message.payload = (void*) buff;
+      message.payloadlen = strlen(buff) + 1;
+      int rc = client->publish(topic, message);
+
+      printf("rc:  %d\r\n", rc);
+      printf("Puslish message: %s\r\n", buff);
+}
+
+void close_mqtt() {
+      closed = true;
+}
 
 I2C i2c( PTD9,PTD8);
 
@@ -98,6 +143,47 @@ int main(){
   FXOS8700CQ_writeRegs(data, 2);
 
   char xbee_reply[4];
+
+
+  wifi = WiFiInterface::get_default_instance();
+      if (!wifi) {
+            printf("ERROR: No WiFiInterface found.\r\n");
+            return -1;
+      }
+
+
+      printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
+      int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+      if (ret != 0) {
+            printf("\nConnection error: %d\r\n", ret);
+            return -1;
+      }
+
+
+      NetworkInterface* net = wifi;
+      MQTTNetwork mqttNetwork(net);
+      MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
+
+      //TODO: revise host to your ip
+      const char* host = "192.168.43.220";
+      printf("Connecting to TCP network...\r\n");
+      int rc = mqttNetwork.connect(host, 1883);
+      if (rc != 0) {
+            printf("Connection error.");
+            return -1;
+      }
+      printf("Successfully connected!\r\n");
+
+      MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+      data.MQTTVersion = 3;
+      data.clientID.cstring = "Mbed";
+
+      if ((rc = client.connect(data)) != 0){
+            printf("Fail to connect MQTT\r\n");
+      }
+      if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
+            printf("Fail to subscribe\r\n");
+      }
 
 
   // XBee setting
@@ -167,6 +253,20 @@ int main(){
   // Setup a serial interrupt function of receiving data from xbee
 
   xbee.attach(xbee_rx_interrupt, Serial::RxIrq);
+
+  printf("Ready to close MQTT Network......\n");
+
+      if ((rc = client.unsubscribe(topic)) != 0) {
+            printf("Failed: rc from unsubscribe was %d\n", rc);
+      }
+      if ((rc = client.disconnect()) != 0) {
+      printf("Failed: rc from disconnect was %d\n", rc);
+      }
+
+      mqttNetwork.disconnect();
+      printf("Successfully closed!\n");
+
+      return 0;
 
 }
 
